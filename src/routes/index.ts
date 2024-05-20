a58f329908ac9f1d0ac4782b4e4cd5adee45fddd
idrissjuanito <idrissjuanito@gmail.com>
2024-05-21 00:00:46 +0100

    added user delete and post routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,12 +1,18 @@
 import { Request, Response } from "express";
 import { Router } from "express";
 import { NotFoundError } from "../errors";
+import {
+  newUser,
+  getAllUsers,
+  deleteUser,
+} from "../controllers/UserController";
+
 const router = Router();
 const userRouter = Router();
 
-userRouter.get("/", (req, res) => {
-  res.json({ state: "all green" });
-});
+userRouter.get("/", getAllUsers);
+userRouter.post("/registration", newUser);
+userRouter.delete("/:userId", deleteUser);
 
 router.use("/users", userRouter);
 
